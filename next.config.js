8fb062b2f51516bfa6260b11a32dafd1d647465a
EngrZaks <abzakariyya@gmail.com>
2023-05-30 23:40:28 +0100

    initial setup for soko for next 13

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: [
+      "firebasestorage.googleapis.com",
+      "lh3.googleusercontent.com",
+      "graph.facebook.com",
+      "ik.imagekit.io",
+      "via.placeholder.com",
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
